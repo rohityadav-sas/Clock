be9d5f8e000328ea64a301ab46534d66412a448b
Rohit Yadav <rohityadav.se@gmail.com>
2023-11-04 02:36:53 +0545

    Code Optimization

--- script.js
+++ script.js
@@ -14,14 +14,11 @@ update();
 setInterval(update, 1000);
 
 function textGlow() {
-    texts.forEach((e, index) => {
-        for (let i = 0; i < 12; i++) {
-            let isColor = (i === index) && ((i * 30 === secondDegree) || (i * 30 === minuteDegree) || (i * 30 === hourDegree));
-            e.classList.toggle('shadow', isColor);
-            if (i * 30 === secondDegree) { e.style.setProperty('--colour', 'red') }
-            else if (i * 30 === minuteDegree) { e.style.setProperty('--colour', 'blue') }
-            else if (i * 30 === hourDegree) { e.style.setProperty('--colour', 'green') };
-            if (isColor) { break; }
-        }
+    texts.forEach((e, i) => {
+        let isColor = (i * 30 === secondDegree) || (i * 30 === minuteDegree) || (i * 30 === hourDegree);
+        e.classList.toggle('shadow', isColor);
+        if (i * 30 === secondDegree) { e.style.setProperty('--colour', 'red') }
+        else if (i * 30 === minuteDegree) { e.style.setProperty('--colour', 'blue') }
+        else if (i * 30 === hourDegree) { e.style.setProperty('--colour', 'green') };
     })
 }
